41e7df8658e4b8ff4d93c655817fb2e99fd2dacb
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-12-17 14:24:50 -0800

    add footer to the web page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Footer() {
+    return(
+        <div>
+            <style jsx>{`
+                h3 {
+                    text-align: center;
+                }
+
+                .footer {
+                    position: absolute;
+                    bottom: 0px;
+                    left: 0px;
+                    width: 100%;
+                    background-color: gray;
+                }
+            `}</style>
+            <div className="footer">
+                <h3>(c) Azamat {new Date().toLocaleDateString()}</h3>
+            </div>
+        </div>
+    )
+}

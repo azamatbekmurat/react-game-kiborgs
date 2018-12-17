4fde483d9290a09855b9394a03cf88449aabce23
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-12-17 00:45:06 -0800

    add App.jsx file with main structure of view of the webpage

---
+++ src/components/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Switch, Route, withRouter, Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import Header from './Header'
+import Footer from './Footer'
+import Navbar from './Navbar'
+import Home from './Home'
+import UserInfo from './UserInfo'
+import Game from './Game'
+
+class App extends React.Component {
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        return(
+            <div>
+                <Header/>
+                <Navbar/>
+                <Switch>
+                    <Route exact path='/' component={Home} />
+                    <Route path='/userinfo' component={UserInfo} />
+                    <Route path='/game' component={Game} />
+                </Switch>
+                <Footer/>
+            </div>
+        )
+    }
+}
+
+export default withRouter(connect()(App));

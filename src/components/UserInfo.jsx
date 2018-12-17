af7e3ae1eb36756e29fb70d7724420ebdd16c678
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2018-12-17 15:45:23 -0800

    add userInfo component

---
+++ src/components/UserInfo.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { PropTypes } from 'prop-types'
+import { Lint } from 'react-router-dom'
+
+function UserInfo(props){
+
+    return(
+        <div>
+            <div>
+                <h1>Welcome to <em>React Androids</em>, {props.currentUserName}!</h1>
+            </div>
+            <div>
+                <Link to="/game"><button type="button">Play Game</button></Link>
+            </div>
+            <style jsx>{`
+            `}
+            </style>
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        currentUserEmail: state.user['email'],
+        currentUserName: state.user['name']
+    }
+}
+
+UserInfo.propTypes = {
+    dispatch: PropTypes.func,
+    currentUserEmail: PropTypes.string,
+    currentUserName: propTypes.string
+}
+
+export default connect(mapStateToProps)(UserInfo)
